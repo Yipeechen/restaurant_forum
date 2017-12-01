a19945e9475626284a30033b2a076955c2170f36
Yipeechen <yipeechen0418@gmail.com>
2017-12-01 19:11:37 +0800

    add pagination using Kaminari gem

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -5,7 +5,7 @@ class Admin::RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
 def index
-  @restaurants = Restaurant.all
+  @restaurants = Restaurant.page(params[:page]).per(10)
 end
 
    def new
