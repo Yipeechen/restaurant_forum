4792ac3210bb8b762fc2a3dfeacefc1a5214d586
Yipeechen <yipeechen0418@gmail.com>
2017-12-29 20:41:16 +0800

    setup route for user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :comments, only: [:create, :destroy]
   end
   
+  resources :users, only: [:show, :edit, :update]
+
   resources :categories, only: :show
   root "restaurants#index"
 
