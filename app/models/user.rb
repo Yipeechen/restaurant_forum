e1bf89fbdbe4ae08e687e078178e8c65f942f1cf
Yipeechen <yipeechen0418@gmail.com>
2018-01-09 14:07:09 +0800

    add user name to registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
 
   has_many :comments
