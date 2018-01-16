9932845df43ce8169cfebf245e6094a923756abf
Yipeechen <yipeechen0418@gmail.com>
2018-01-16 16:45:36 +0800

    add is_like? method to restaurant model for checking like/unlike button

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -17,4 +17,8 @@ class Restaurant < ApplicationRecord
     has_many :likes, dependent: :destroy
     has_many :liked_users, through: :likes, source: :user
 
+    def is_liked?(user)
+      self.liked_users.include?(user)   
+    end
+
 end
