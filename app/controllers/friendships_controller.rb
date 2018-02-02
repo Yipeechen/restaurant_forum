e3ea968e893c16150f5b5fc23f77e86481000dfc
Yipeechen <yipeechen0418@gmail.com>
2018-02-02 15:28:23 +0800

    add create action for friendship record

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,14 @@
 class FriendshipsController < ApplicationController
+
+  def create
+    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+
+    if @friendship.save
+      flash[:notice] = "Friend request successfully sent!" 
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @friendship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end 
+  end
 end
